b0ec82a12d478f76cb956d53966339c5b32495ba
liuzhijie <liuzhijie@cyou-inc.com>
2017-07-28 18:45:29 +0800

    1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+	private readonly int ROW_L = 6;
 	public Image[] locks;
 	public LineRenderer lr;
 	// Use this for initialization
@@ -19,7 +20,46 @@ public class GameManager : MonoBehaviour {
 	public void RandLine()
 	{
 		int r = Random.Range (0, locks.Length);
-		Debug.LogError (r);
+		Debug.Log ("r=" + r);
+		if (isLeftTop (r)) {
+			Debug.Log ("LT");
+			return;
+		}
+
+		if (isLeftBottom (r)) {
+			Debug.Log ("LB");
+			return;
+		}
+
+		if (isRightTop (r)) {
+			Debug.Log ("RT");
+			return;
+		}
+
+		if (isRightBottom (r)) {
+			Debug.Log ("RB");
+			return;
+		}
+
+		if (isLeft (r)) {
+			Debug.Log ("L");
+			return;
+		} 
+
+		if (isTop (r)) {
+			Debug.Log ("T");
+			return;
+		}
+
+		if (isRight (r)) {
+			Debug.Log ("R");
+			return;
+		}
+
+		if (isBottom (r)) {
+			Debug.Log ("B");
+			return;
+		}
 	}
 
 	public void DrawLine ()
@@ -32,5 +72,45 @@ public class GameManager : MonoBehaviour {
 //				lr.gameObject.transform.position = v;
 			lr.SetPosition (i,v);
 		}
+	}
+
+	private bool isLeftTop(int r)
+	{
+		return isLeft (r) && isTop (r);
+	}
+
+	private bool isLeftBottom(int r)
+	{
+		return isLeft (r) && isBottom (r);
+	}
+
+	private bool isRightTop(int r)
+	{
+		return isRight (r) && isTop (r);
+	}
+
+	private bool isRightBottom(int r)
+	{
+		return isRight (r) && isBottom (r);
+	}
+
+	private bool isLeft(int r)
+	{
+		return r % ROW_L == 0;
+	}
+
+	private bool isRight(int r)
+	{
+		return (r + 1) % ROW_L == 0;
+	}
+
+	private bool isTop(int r)
+	{
+		return r / ROW_L == 0;
+	}
+
+	private bool isBottom(int r)
+	{
+		return r / ROW_L == (locks.Length - 1) / ROW_L;
 	}
 }
