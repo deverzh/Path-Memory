f97541acf6e1d3a53569d69e35fc5b9c05465ac2
liuzhijie <liuzhijie@cyou-inc.com>
2017-08-12 22:44:19 +0800

    1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,60 +3,54 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-	private readonly int ROW_L = 6;
 	public Image[] locks;
 	public LineRenderer lr;
-	// Use this for initialization
 	public int[] points;
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
 	
 	}
 
 	public void RandLine()
 	{
 		int r = Random.Range (0, locks.Length);
+		r = 0;
 		Debug.Log ("r=" + r);
-		if (isLeftTop (r)) {
-			Debug.Log ("LT");
+		if (RandomPath.Inst.isLeftTop (r)) {
+			Debug.Log ("LT"+RandomPath.Inst.LTNext(r));
 			return;
 		}
 
-		if (isLeftBottom (r)) {
+		if (RandomPath.Inst.isLeftBottom (r)) {
 			Debug.Log ("LB");
 			return;
 		}
 
-		if (isRightTop (r)) {
+		if (RandomPath.Inst.isRightTop (r)) {
 			Debug.Log ("RT");
 			return;
 		}
 
-		if (isRightBottom (r)) {
+		if (RandomPath.Inst.isRightBottom (r)) {
 			Debug.Log ("RB");
 			return;
 		}
 
-		if (isLeft (r)) {
+		if (RandomPath.Inst.isLeft (r)) {
 			Debug.Log ("L");
 			return;
 		} 
 
-		if (isTop (r)) {
+		if (RandomPath.Inst.isTop (r)) {
 			Debug.Log ("T");
 			return;
 		}
 
-		if (isRight (r)) {
+		if (RandomPath.Inst.isRight (r)) {
 			Debug.Log ("R");
 			return;
 		}
 
-		if (isBottom (r)) {
+		if (RandomPath.Inst.isBottom (r)) {
 			Debug.Log ("B");
 			return;
 		}
@@ -74,43 +68,5 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
-	private bool isLeftTop(int r)
-	{
-		return isLeft (r) && isTop (r);
-	}
 
-	private bool isLeftBottom(int r)
-	{
-		return isLeft (r) && isBottom (r);
-	}
-
-	private bool isRightTop(int r)
-	{
-		return isRight (r) && isTop (r);
-	}
-
-	private bool isRightBottom(int r)
-	{
-		return isRight (r) && isBottom (r);
-	}
-
-	private bool isLeft(int r)
-	{
-		return r % ROW_L == 0;
-	}
-
-	private bool isRight(int r)
-	{
-		return (r + 1) % ROW_L == 0;
-	}
-
-	private bool isTop(int r)
-	{
-		return r / ROW_L == 0;
-	}
-
-	private bool isBottom(int r)
-	{
-		return r / ROW_L == (locks.Length - 1) / ROW_L;
-	}
 }
---
+++ Assets/Scripts/RandomPath.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomPath {
+
+	public static readonly int ROW_L = 6;
+	public static readonly int COL_L = 3;
+
+	private enum NextPos{
+		NR  = 1,
+		NT  = -ROW_L,
+		NB  = ROW_L,
+		NL  = -1,
+		NLT = -1-ROW_L,
+		NRT = 1-ROW_L,
+		NRB = 1+ROW_L,
+		NLB = -1+ ROW_L
+	}
+
+	private static RandomPath _instance;
+
+	public static RandomPath Inst{
+		get{
+			if (_instance == null) {
+				_instance = new RandomPath ();
+			}
+			return _instance;
+		}
+	}
+
+	public int LTNext(int r)
+	{
+		List<int> list = new List<int> ();
+		list.Add (r + NextPos.NR);
+		list.Add (r + NextPos.NB);
+		list.Add (r + NextPos.NRB);
+		for (int i = r + NextPos.NRB + NextPos.NR; i < r + NextPos.NRB + ROW_L - r % ROW_L; i++) {
+			list.Add (i);
+		}
+		for (int i = 0; i < COL_L - r / ROW_L - 1; i++) {
+			list.Add (r + NextPos.NRB + NextPos.NR +NextPos.NR * i);
+		}
+		Random rm = new Random();
+		int idx = rm.Next (list.Count);  //随机数最大值不能超过list的总数
+
+		return list[idx];
+	}
+
+	public bool isLeftTop(int r)
+	{
+		return isLeft (r) && isTop (r);
+	}
+
+	public bool isLeftBottom(int r)
+	{
+		return isLeft (r) && isBottom (r);
+	}
+
+	public bool isRightTop(int r)
+	{
+		return isRight (r) && isTop (r);
+	}
+
+	public bool isRightBottom(int r)
+	{
+		return isRight (r) && isBottom (r);
+	}
+
+	public bool isLeft(int r)
+	{
+		return r % ROW_L == 0;
+	}
+
+	public bool isRight(int r)
+	{
+		return (r + 1) % ROW_L == 0;
+	}
+
+	public bool isTop(int r)
+	{
+		return r / ROW_L == 0;
+	}
+
+	public bool isBottom(int r)
+	{
+		return r / ROW_L == (ROW_L*COL_L - 1) / ROW_L;
+	}
+
+}
